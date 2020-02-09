7e32e470f1e7b08013885f91a9de4734049dbbce
tbriany <brianytaveras@pursuit.org>
2020-02-09 17:37:55 -0500

    created component that will display a shows info and comments, as well as form to add new comments

---
+++ frontend/src/Components/ShowProfile.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const ShowProfile = () => {
+    return (
+        <div>
+            <h1>Welcome to Shows Profile Page</h1>
+        </div>
+    )
+}
+
+export default ShowProfile;
