6565c70575123bf4a7166da84c01d510169d3f17
tbriany <brianytaveras@pursuit.org>
2020-02-09 17:36:47 -0500

    created component that will display form to add a new show

---
+++ frontend/src/Components/NewShowForm.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const AddShow = () => {
+    return (
+        <div>
+            <h1>New Show Form will go hereee</h1>
+        </div>
+    )
+}
+
+export default AddShow;
