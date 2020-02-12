b794ab64ef429ffac602b37b88caa0a793b27f7e
tbriany <brianytaveras@pursuit.org>
2020-02-11 20:39:37 -0500

    display each show on users page as a link with a photo and title

--- frontend/src/Components/Helpers/ShowsDisplay.jsx
+++ frontend/src/Components/Helpers/ShowsDisplay.jsx
@@ -1,28 +1,35 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 
-const ShowsDisplay = () => {
-// pass props to check if its displaying the show on 
-// the users profile page or on the shows endpoint displaying all shows 
- if ("displayingAllShows"){
 
-    return (
-        <li>
-            <img></img>
-            <h4>Title</h4>
-            <p>Genre</p>
-            <p>Being watched by</p>
-        </li>
-    )
- } else {
-    return (
-        <li>
-            <img></img>
-            <h4>Title</h4>
-            <p>Genre</p>
-        </li>
-    )
- }
-
+const ShowsDisplay = (props) => {
+    const { title, img, genre, userID, showID } = props
+    // pass props to check if its displaying the show on 
+    // the users profile page or on the shows endpoint displaying all shows 
+    if (userID) {
+        return (
+            <li>
+                <Link to={`/shows/${showID}/user/${userID}`}>
+                    <div>
+                        <img src={img} alt="show avatar"></img>
+                        <h4>{title}</h4>
+                        <p>{genre}</p>
+                    </div>
+                </Link>
+            </li>
+        )
+    } else {
+        return (
+            <li>
+                <img></img>
+                <h4>Title</h4>
+                <p>Genre</p>
+                <p>Being watched by</p>
+            </li>
+        )
+    }
 }
+
+
 
 export default ShowsDisplay;
