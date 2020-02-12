4d90437cd73992e0cdc8970da5b90c8f92d94765
tbriany <brianytaveras@pursuit.org>
2020-02-11 20:38:53 -0500

    display comments as an li

--- frontend/src/Components/Helpers/CommentsDisplay.jsx
+++ frontend/src/Components/Helpers/CommentsDisplay.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 
-const CommentsDisplay = () => {
+const CommentsDisplay = (props) => {
+    const {comment, username} = props
     return (
         <li>
-            <h4>Username</h4>
-            <p>comment</p>
+            <h4>{username}</h4>
+            <p>{comment}</p>
         </li>
     )
 }
