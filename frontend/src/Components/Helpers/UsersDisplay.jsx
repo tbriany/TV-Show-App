999263a66731b3bfdc1e57a341d16812538ff468
tbriany <brianytaveras@pursuit.org>
2020-02-12 22:30:46 -0500

    displayed logged in for hard coded logged in user

--- frontend/src/Components/Helpers/UsersDisplay.jsx
+++ frontend/src/Components/Helpers/UsersDisplay.jsx
@@ -2,17 +2,31 @@ import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 
 const UserDisplay = (props) => {
-    const {username, img, id} = props
-    return (
-        <li>
-        <Link to={`/users/${id}`}>
-        <div>
-         <img src={img} alt="user avatar"></img><br></br>
-         <h3>{username}</h3>
-        </div>
-        </Link>
-        </li>
-    )
+    const { username, img, id } = props
+    if (id === 5) {
+        return (
+            <li>
+                <Link to={`/users/${id}`}>
+                    <div>
+                        <img src={img} alt="user avatar"></img><br></br>
+                        <h3>{username}</h3>
+                        <p>Logged In</p>
+                    </div>
+                </Link>
+            </li>
+        )
+    } else {
+        return (
+            <li>
+                <Link to={`/users/${id}`}>
+                    <div>
+                        <img src={img} alt="user avatar"></img><br></br>
+                        <h3>{username}</h3>
+                    </div>
+                </Link>
+            </li>
+        )
+    }
 }
 
 export default UserDisplay;
